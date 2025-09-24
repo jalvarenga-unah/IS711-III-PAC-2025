b83fa3b21a18076e6c3a66a1282fdc322f908629
jalvarenga11 <juan.alvarenga@ess.hn>
2025-09-24 17:48:46 -0600

    Manejo de promesas pt 2

--- intro-js/05-promesas.js
+++ intro-js/05-promesas.js
@@ -37,16 +37,16 @@ const mascota1 = { nombre: 'Firulais', raza: 'Pastor Alemán', edad: 5 }
 const mascota2 = { nombre: 'Michi', raza: 'Siames', edad: 3 }
 const mascota3 = { nombre: 'Nemo', raza: 'Pez Payaso' }
 
-console.log('Inicio de la creación de la mascota')
-crearMascota(mascota1)
+console.log('Inicio de la creación de la mascota');
+/*crearMascota(mascota1)
     .then((respuesta) => {
         console.log('Mascota1 creada:', respuesta)
     })
     .catch((error) => {
         console.error('Mascota1 ' + error)
     })
-
-
+ 
+ 
 crearMascota(mascota2)
     .then((respuesta) => {
         console.log('Mascota2 creada:', respuesta)
@@ -54,6 +54,17 @@ crearMascota(mascota2)
     .catch((error) => {
         console.error('Mascota2 ' + error)
     })
+ 
+crearMascota(mascota3)
+    .then((respuesta) => {
+        console.log('Mascota3 creada:', respuesta)
+    })
+    .catch((error) => {
+        console.error('Mascota3 ' + error)
+ 
+    })
+*/
+
 
 crearMascota(mascota3)
     .then((respuesta) => {
@@ -62,6 +73,56 @@ crearMascota(mascota3)
     .catch((error) => {
         console.error('Mascota3 ' + error)
 
-    })
+    }).finally(() => {
+        // siempre se ejecuta al finalizar la promesa
+        console.log('Final de la creación de la mascota')
+    });
 
-console.log('Final de la creación de la mascota')
+// resuelve las promesas de forma paralela
+//? Resolución paralela de promesas (asincrónica)
+// Promise.all([
+//     crearMascota(mascota1),
+//     crearMascota(mascota2),
+//     crearMascota(mascota3),
+// ]).then((resultados) => {
+//     // Todas las promesas se resolvieron correctamente
+//     console.log(resultados)
+
+// }).catch((error) => {
+//     console.log(error)
+//     console.error('Error en alguna de las promesas')
+// })
+
+
+(async () => {
+    console.log('creando mascota 1')
+    try {
+        await crearMascota(mascota1)
+    } catch (error) {
+        console.error('Mascota1 ' + error)
+    }
+
+    try {
+        console.log('creando mascota 2')
+        await crearMascota(mascota2)
+    } catch (error) {
+        console.error('Mascota2 ' + error)
+    }
+
+
+    try {
+        console.log('creando mascota 3')
+        await crearMascota(mascota3)
+    } catch (error) {
+        console.error('Mascota3 ' + error)
+    }
+
+    console.log('Final de la creación de la mascota')
+})()
+
+
+// const [respuesta1, respuesta2, respuesta3] = await Promise.all([
+//     crearMascota(mascota1),
+//     crearMascota(mascota2),
+//     crearMascota(mascota3),
+// ])
