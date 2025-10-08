f728c41f7a88d2ac126b733284af3989bab295d1
jalvarenga11 <juan.alvarenga@ess.hn>
2025-10-08 17:53:16 -0600

    definiendo rutas con http

--- server-node/index.js
+++ server-node/index.js
@@ -9,15 +9,43 @@ loadEnvFile() // carga las variables de entorno desde el archivo .env
 // ya es mi servidor web
 const app = createServer((req, res) => {
 
-    console.log(req.url)
-    // console.log(req.headers)
-    console.log(req.body)
-    console.log(req.method)
+    //GET: consulto
+    //POST: creo
+    //PUT: actualizo
+    //DELETE: elimino
 
-    // 4. Se le da respuesta al usuario
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end('{"message":"Hola mundo desde node js"}') // finalizar la petición
-    // res.end('<h1>Hola mundo</h1>') // finalizar la petición
+    switch (req.method) {
+        case 'GET':
+            console.log('El usuario está consultando información')
+
+            switch (req.url) {
+
+                case '/todos':
+                    // res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end('{"message":"se obtuvieron todas las tareas"') // finalizar la petición
+                    break;
+                case '/todos/abc123':
+                    res.writeHead(200, { 'Content-Type': 'application/json' });
+                    res.end('{"message":"se obtuvo la tarea con id abc123"}') // finalizar la petición
+                    break;
+                default:
+                    res.writeHead(404, { 'Content-Type': 'application/json' });
+                    res.end('{"message":"ruta no encontrada"}') // finalizar la petición
+                    break;
+            }
+
+            break;
+        case 'POST':
+            console.log(req.body)
+            console.log('El usuario está creando información')
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end('{"message":"se creó una nueva tarea"}') // finalizar la petición
+            break;
+        default:
+            res.writeHead(405, { 'Content-Type': 'application/json' });
+            res.end('{"message":"método no permitido"}') // finalizar la petición
+            break;
+    }
 
 })
 
