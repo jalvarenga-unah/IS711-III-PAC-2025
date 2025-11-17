1f7abd36f7bf2fb25a2311255acdc830e8c6eaad
jalvarenga11 <juan.alvarenga@ess.hn>
2025-11-17 17:53:10 -0600

    manejando politcas de cors

--- api-todos/index.js
+++ api-todos/index.js
@@ -1,6 +1,7 @@
 import express from 'express'; // equivalente al createServer de node:http
+import cors from 'cors'
 import { loadEnvFile } from 'node:process';
-
+import { rateLimit } from 'express-rate-limit'
 import todosRouter from './routes/todos.routes.js'
 import { config } from 'zod'
 import { es } from "zod/locales"
@@ -12,6 +13,29 @@ config(es())
 loadEnvFile() // carga las variables de entorno desde el archivo .env
 
 const app = express() // <- No requiere la req, res
+
+app.use(cors({
+    origin: [
+        'http://127.0.0.1:5500',
+        'https://test.misuperapp.com',
+        'https://misuperapp.com',
+    ],
+    methods: 'GET,POST, PUT, OPTIONS'
+})) // permitiendo todos los origenes para consumir el API
+
+
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 15 minutes
+    limit: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    message: {
+        message: 'Oye Oye, tranquilo viejo'
+    }
+})
+
+app.use(limiter)
 
 // Middlewares -> una acción o un metodo, que intercepta las peticiones
 app.use(express.json()) // transformar el body de la petición a un formarto json
